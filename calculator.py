6cc6552394b6948f622212cdecdfeeb8dcc755ac
parkdanyong <danyong2001@gmail.com>
2024-05-16 23:50:02 +0900

    Add get_Abs function

--- calculator.py
+++ calculator.py
@@ -11,3 +11,9 @@ def get_Median(a, b) :
     return (a + b) / 2
 def get_Remainder(a, b) :
     return a // b
+def get_Abs(num) :
+    if num > 0 :
+        return num
+    else :
+        return -num
+
