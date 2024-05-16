7e392ee4cac3d88a18aae6efc382fff0b24bc20c
parkdanyong <danyong2001@gmail.com>
2024-05-17 00:04:51 +0900

    Add get_Sum_ver1 function

--- calculator.py
+++ calculator.py
@@ -9,6 +9,7 @@ def divide_new(a, b) :
     return a / b
 def get_Median(a, b) :
     return (a + b) / 2
+<<<<<<< HEAD
 def get_Remainder(a, b) :
     return a // b
 def get_Abs(num) :
@@ -18,3 +19,5 @@ def get_Abs(num) :
         return -num
 def get_Percent(a, b) :
     return (a / b) * 100
+def get_Sum_ver1(n) :
+    return n(n + 1)/2
